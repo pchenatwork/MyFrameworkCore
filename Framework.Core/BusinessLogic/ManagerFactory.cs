d0f65f6c2cf9ba5a25fdc4f207f9be83d7a42cdc
PAUL CHEN <pchenatwork@hotmail.com>
2021-06-21 02:17:19 -0400

    Solution fixes

---
+++ Framework.Core/BusinessLogic/ManagerFactory.cs
@@ -0,0 +1,46 @@
+﻿using Framework.Core.Common;
+using Framework.Core.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework.Core.BusinessLogic
+{
+    public sealed class ManagerFactory2<T> : FactoryBase<ManagerFactory2<T>> where T: IValueObject
+    {
+        #region	Constructors
+        static ManagerFactory2()
+        {
+         ///    _logger = LoggingUtility.GetLogger(typeof(ManagerFactory<T>).FullName);
+        }
+        #endregion Constructors
+
+        #region	Public Methods
+        /// <summary>
+        /// Factory creation method for Manager instances
+        /// </summary>
+        /// <returns>
+        /// an instance of Manager
+        /// </returns>
+        public IManager<T> GetManager()
+        {
+            return (IManager<T>)Activator.CreateInstance(typeof(IManager<T>));
+        }
+
+        /// <summary>
+        /// Factory creation method for Manager instances
+        /// </summary>
+        /// <returns>
+        /// an instance of Manager
+        /// </returns>
+        public IManager<T> GetManager(string dataSourceName)
+        {
+            return (IManager<T>)Activator.CreateInstance(typeof(IManager<T>), new object[] { dataSourceName });
+            //return (T)Activator.CreateInstance(typeof(T),
+            //    BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance, null,
+            //    new object[] { dataSourceName });
+        }
+        #endregion Public Methods
+
+    }
+}
---
+++ Framework.Core/BusinessLogic/UtilityBase.cs
@@ -0,0 +1,41 @@
+﻿using Framework.Core.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework.Core.BusinessLogic
+{
+    public class UtilityBase<T> where T: IValueObject
+	{
+		public static T CreateObject()
+		{
+			return (T)Activator.CreateInstance(typeof(T));
+		}
+		public static T Get(string dataSourceName, int id)
+		{
+			IManager<T> mgr = ManagerFactory2<T>.Instance.GetManager(dataSourceName);
+			return (T)mgr.Get(id);
+		}
+		public static ValueObjectCollection<T> FindByCriteria(string dataSourceName, string finderType, object[] criteria)
+		{
+			IManager<T> mgr = ManagerFactory2<T>.Instance.GetManager(dataSourceName);
+			return (ValueObjectCollection<T>)mgr.FindByCriteria(finderType, criteria);
+		}
+		public static int Create(string dataSourceName, T v)
+		{
+			IManager<T> mgr = ManagerFactory2<T>.Instance.GetManager(dataSourceName);
+			return mgr.Create(v);
+		}
+		public static bool Update(string dataSourceName, T v)
+		{
+			IManager<T> mgr = ManagerFactory2<T>.Instance.GetManager(dataSourceName);
+			return mgr.Update(v);
+		}
+		public static bool Delete(string dataSourceName, int id)
+		{
+			IManager<T> mgr = ManagerFactory2<T>.Instance.GetManager(dataSourceName);
+			return mgr.Delete(id);
+		}
+
+	}
+}
